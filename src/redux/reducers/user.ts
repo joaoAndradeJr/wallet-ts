b8835a94936696d16e325d4d7620d87ae877ad77
joaoAndradeJr <fullservicemanaus@gmail.com>
2024-05-22 14:18:24 -0300

    add: Redux configuration

--- src/redux/reducers/user.ts
+++ src/redux/reducers/user.ts
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { AnyAction } from 'redux';
+
+const INITIAL_STATE = {
+  user: null,
+};
+
+function user(state = INITIAL_STATE, action: AnyAction) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default user;
--- src/redux/reducers/wallet.ts
+++ src/redux/reducers/wallet.ts
@@ -1,1 +1,17 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { AnyAction } from 'redux';
+
+const INITIAL_STATE = {
+  expenses: [],
+  currencies: [],
+  idToEdit: 0,
+  isEditing: false,
+};
+
+function wallet(state = INITIAL_STATE, action: AnyAction) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default wallet;
